5282fe7932a026f0937c42961c59bf0b7b1b3602
Cael <caeltindoy@gmail.com>
2026-07-15 20:16:41 +0800

    Implement data restructuring using array map

--- script.js
+++
@@ -1,7 +0,0 @@
-let john = { name: "John", surname: "Smith", id: 1 };
-let pete = { name: "Pete", surname: "Hunt", id: 2 };
-let mary = { name: "Mary", surname: "Key", id: 3 };
-
-let users = [ john, pete, mary ];
-
-let usersMapped = /* ... your code ... */
